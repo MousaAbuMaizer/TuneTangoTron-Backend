e76ca24365a87d166ec856394876846b7638cdf1
hamzaqasemtest <144328670+hamzaqasemtest@users.noreply.github.com>
2024-03-14 11:55:00 +0300

    fadfadscads

--- app/llm/llm_class.py
+++ app/llm/llm_class.py
@@ -29,30 +29,3 @@ class GenerateSyntheticData:
 
         print(preview_prompt_response)
 
-
-
-AZURE_API_KEY_MAIN = "6edbfb029e8c4a0c8a13e065c3c76553"
-AZURE_END_POINT_MAIN = "https://vidjalsa.openai.azure.com/openai/deployments/VidJalsa/chat/completions?api-version=2024-02-15-preview"
-
-azure_llm = AzureChatOpenAI(
-    api_key=AZURE_API_KEY_MAIN,
-    model="gpt-4-1106-preview",
-    openai_api_version="2023-07-01-preview",
-    azure_endpoint=AZURE_END_POINT_MAIN,
-    temperature=0.55
-)
-
-GenerateSyntheticData = GenerateSyntheticData(azure_llm)
-GenerateSyntheticData.generate_data(
-    topic= "car",
-    instructions= """
-    the dataset should have Systemmessage, HumanMessage and AssistantMessage. the SystemMessage should be "you are a useful chatbot requeired to answer questions about the history of the world" and the HumanMessage should the a query about the history of the world  and the AssistantMessage is the bot response on that query.
-    """,
-    prefix= "",
-    example= """
-    {"SystemMessage": " you are a useful chatbot requeired to answer questions about the history of the world", 
-    "HumanMessage": "what is the history of the world", 
-    "AssistantMessage": "The history of the world is the history of humanity, as determined from archaeology, anthropology, genetics, linguistics, and other disciplines; and, for periods since the invention of writing, from recorded history and from secondary sources and studies."}
-    """,
-    number_of_records= 5
-)
